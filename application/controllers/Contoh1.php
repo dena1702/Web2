347ccb1336d0330c360f4c603be3092aa29a32fc
dena1702 <denadena044@gmail.com>
2021-10-22 05:34:10 -0700

    Menambahkan Database, Studi kasus Pustaka Booking, Menambahkan review pertemuan 7 dan pertemuan 9

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Dena Febi Anggraini
+            Saya tinggal di daerah sintang
+            olahraga yang saya sukai adalah
+            Badminton";
+    }
+}
---
+++ application/views/v_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>web prog II | Merancang Template Sederhana dengan codeigniter</title>
+    <link rel="stylesheeet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <headeer>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan codeigniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url() . 'web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url() . 'web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
